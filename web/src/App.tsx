818f4087a68532695c9da866d1ce960bd6de5cad
Ty Morris <tirey@hey.com>
2026-06-03 19:57:27 -0500

    mount CleanButton in app topbar with wrap styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import CleanButton from "./components/CleanButton";
 import UpdateButton from "./components/UpdateButton";
 
 export default function App() {
   return (
     <main className="app-main">
       <header className="app-topbar">
+        <CleanButton />
         <UpdateButton />
       </header>
       <div className="app-content">
